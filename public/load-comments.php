6a93d064038703e21d895f727d11059ce87d1560
Ricardo199 <ricardo1529@gmail.com>
2023-10-13 00:19:13 -0600

    el encabezado se repetia cada dos filas

--- public/load-comments.php
+++ public/load-comments.php
@@ -10,17 +10,17 @@ $sql = "SELECT author, comentario FROM comments LIMIT $commentsNewCount, 2";
 $result = mysqli_query($conn, $sql);
 
 // Verifica si hay filas en el resultado
-if (mysqli_num_rows($result) >= 0) {
+if (mysqli_num_rows($result) > 0) {
     // Si hay filas, comienza a generar una tabla HTML
 
-    // Muestra las etiquetas de apertura de la tabla
+    // Muestra las etiquetas de apertura de la tabla y el encabezado una vez
     echo "<table class='w-full whitespace-no-wrap'>
         <thead>
             <tr class='text-xs font-semibold tracking-wide text-left text-gray-500 uppercase border-b dark:border-gray-700 bg-gray-50 dark:text-gray-400 dark:bg-gray-800'>
                 <th class='px-4 py-3'>Cliente</th>
                 <th class='px-4 py-3'>Monto</th>
                 <th class='px-4 py-3'>Estado</th>
-                <th class='px-4 py-3'>Commentarios</th>
+                <th class='px-4 py-3'>Comentarios</th>
             </tr>
         </thead>
         <tbody class='bg-white divide-y dark:divide-gray-700 dark:bg-gray-800'>";
@@ -33,7 +33,7 @@ if (mysqli_num_rows($result) >= 0) {
                 <div class='flex items-center text-sm'>
                     <!-- Avatar con sombra interior -->
                     <div class='relative hidden w-8 h-8 mr-3 rounded-full md:block'>
-                        <img class='object-cover w-full h-full rounded-full' src='https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1?q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjE3Nzg0fQ' alt='' loading='lazy' />
+                        <img class='object-cover w-full h-full rounded-full' src='https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1?q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjE3N7.00' alt='' loading='lazy' />
                         <div class='absolute inset-0 rounded-full shadow-inner' aria-hidden='true'></div>
                     </div>
                     <div>
@@ -60,4 +60,3 @@ if (mysqli_num_rows($result) >= 0) {
 // Cierra la conexión a la base de datos al final del script
 mysqli_close($conn);
 ?>
-
