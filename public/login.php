50c8e7201e2b6b8b8d73087eef37b833edcfd338
Brayan Prado <ia.bprado@ufg.edu.sv>
2023-10-13 00:25:28 -0600

    archivos y eventos necesarios para hacer login

---
+++ public/login.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+require('dbh.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = mysqli_real_escape_string($conn, $_POST['email']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+
+    $sql = "SELECT * FROM usuarios WHERE correo = '$username' and contrasena = '$password'";
+    $result = mysqli_query($conn, $sql);
+    $count = mysqli_num_rows($result);
+
+    if ($count == 1) {
+        $_SESSION['login_user'] = $username;
+        header("location: index.php"); // Redirige a la página de inicio después del inicio de sesión exitoso
+    } else {
+        $error = "Usuario o contraseña incorrectos";
+        header("location: pages/login.html?error=" . urlencode($error));
+    }
+}
+?>
